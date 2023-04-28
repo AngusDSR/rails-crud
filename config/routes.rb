2f8b64978cc5389d7bc74393166b1a17f325ea5b
Angus Spawton-Rice <adspawtonrice@gmail.com>
2023-04-28 10:41:36 +0100

    CRU routes, views and controllers added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "restaurants", to: "restaurants#index"
+  get "restaurants/new", to: "restaurants#new"
+  get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  get "restaurants/:id/edit", to: "restaurants#edit"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  post "restaurants", to: "restaurants#create"
+  patch "restaurants/:id", to: "restaurants#update"
 end
