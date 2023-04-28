6bbfb4bd6300b9a5f0e3545bcc72f8efba61ebb0
Angus Spawton-Rice <adspawtonrice@gmail.com>
2023-04-28 11:05:47 +0100

    Added destroy functionality; links between; refactoring

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,11 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: %i[show edit update destroy]
+
   def index
     @restaurants = Restaurant.all
   end
 
-  def show
-    @restaurant = Restaurant.find(params[:id])
-  end
+  def show; end
 
   def new
     @restaurant = Restaurant.new
@@ -17,20 +17,26 @@ class RestaurantsController < ApplicationController
     redirect_to restaurant_path(restaurant)
   end
 
-  def edit
-    @restaurant = Restaurant.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @restaurant = Restaurant.find(params[:id])
     @restaurant.update(restaurant_params)
     @restaurant.save
     redirect_to restaurant_path(@restaurant)
+  end
+
+  def destroy
+    @restaurant.destroy
+    redirect_to restaurants_path, status: :see_other
   end
 
   private
 
   def restaurant_params
     params.require(:restaurant).permit(:name, :address, :rating)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   get "restaurants", to: "restaurants#index"
   get "restaurants/new", to: "restaurants#new"
   get "restaurants/:id", to: "restaurants#show", as: :restaurant
-  get "restaurants/:id/edit", to: "restaurants#edit"
+  get "restaurants/:id/edit", to: "restaurants#edit", as: :restaurant_edit
 
   post "restaurants", to: "restaurants#create"
   patch "restaurants/:id", to: "restaurants#update"
+  delete "restaurants/:id", to: "restaurants#destroy"
 end
